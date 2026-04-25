423881a92f45e0362c66b88d7bd6a1ef8d988ad4
Givi Phirtskhalava <givi@glitch.ge>
2026-04-25 12:36:40 +0400

    migration file

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260423_134230 from './20260423_134230';
+import * as migration_20260425_083636 from './20260425_083636';
 
 export const migrations = [
   {
     up: migration_20260423_134230.up,
     down: migration_20260423_134230.down,
-    name: '20260423_134230'
+    name: '20260423_134230',
+  },
+  {
+    up: migration_20260425_083636.up,
+    down: migration_20260425_083636.down,
+    name: '20260425_083636'
   },
 ];
